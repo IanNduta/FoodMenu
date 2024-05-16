eaeef28df9dbb8542d0b3317f2d6fdaab04f24f0
Ian Nduta <gathonjiandutai@gmail.com>
2024-05-16 09:40:26 -0400

    finished

--- scripts/site.js
+++ scripts/site.js
@@ -1,34 +1,61 @@
 "use strict";
 let menu = {
-    drinks : [
-     "Water", "Tea", "Sweet Tea",
-     "Coke", "Dr. Pepper", "Sprite"
-     ],
-    entrees : [
-    "Hamburger w/ Fries",
-     "Grilled Cheese w/ Tater Tots",
-    "Grilled Chicken w/ Veggies",
-     "Chicken Fried Steak w/ Mashed Potatoes",
-    "Fried Shrimp w/ Coleslaw",
-     "Veggie Plate"
+    drinks: [
+        "Water", "Tea", "Sweet Tea",
+        "Coke", "Dr. Pepper", "Sprite"
+    ],
+    entrees: [
+        "Hamburger w/ Fries",
+        "Grilled Cheese w/ Tater Tots",
+        "Grilled Chicken w/ Veggies",
+        "Chicken Fried Steak w/ Mashed Potatoes",
+        "Fried Shrimp w/ Coleslaw",
+        "Veggie Plate"
     ],
     desserts: [
-     "Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"
-     ]
-    };
+        "Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"
+    ]
+};
 //input
-    const menuSelection = document.getElementById("menuSelection");
-    const menuList = document.getElementById("otherMenuSelection");
-//output
-    const mealSelectedOutput = document.getElementById("outputMeal");
-//button
-    const submitBtn = document.getElementById("submitBtn");
+const customerMenuCategory = document.getElementById("menuCategory");
+const menuList = document.getElementById("MenuSelection");
 
-window.onload = function(){
-    submitBtn.onclick = onClickeSubmitBtn;
-    initmenuSelectionItems()
+window.onload = function () {
+    customerMenuCategory.onchange = onMenuSelectionChanged;
 }
 
-function onClickeSubmitBtn(){
-    console.log("push");
+function onMenuSelectionChanged() {
+    //what category did the user select?
+    let categorySelected = customerMenuCategory.value;
+    console.log(categorySelected);
+
+    if (categorySelected == null) {
+        console.log("no category was selected");
+    }
+    else if (categorySelected == "drinks") {
+        displayItemsInList(menu.drinks);
+    }
+    else if (categorySelected == "entrees") {
+        displayItemsInList(menu.entrees);
+    }
+    else if (categorySelected == "desserts") {
+        displayItemsInList(menu.desserts);
+    }
+    //using that category, find a list of menu items that are in that category
+
+    //using that list of menu items in the category, display those into the listbox.
+}
+
+function displayItemsInList(arrayOfStrings) {
+    //clear any existing items in the listbox..
+    menuList.options.length = 0;
+    //loop through this array of strings and add all the items to the listbox
+
+    for (let i = 0; i < arrayOfStrings.length; i++) {
+        let newOptions = document.createElement("option");
+        newOptions.value = arrayOfStrings[i];
+        newOptions.innerHTML = arrayOfStrings[i];
+        menuList.appendChild(newOptions);
+    }
+
 }
